c18be8a22e26137b5bc2033b1cce777749e32e02
AzizMakhmudov <azizmakhmudov2412@gmail.com>
2023-02-14 22:57:40 +0500

    fix bugs

--- js/main.js
+++ js/main.js
@@ -34,3 +34,22 @@ $(".calculate-btn").on("click", function () {
   $(".display").val(eval(expression));
 });
 
+$(document).ready(function () {
+  $("#only_num").keydown(function (event) {
+
+    if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+
+      (event.keyCode == 65 && event.ctrlKey === true) ||
+
+      (event.keyCode >= 35 && event.keyCode <= 39)) {
+
+      return;
+    } else {
+
+      if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+        event.preventDefault();
+
+      }
+    }
+  });
+});
